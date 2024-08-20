90514d28314735009a505ce0cc43842de7222d75
FacuZarate605 <facu605@hotmail.com>
2024-08-20 16:44:00 -0300

    boca

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hola Mundo'
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Mundo'
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
